ea2b641da26214b6d498326853a89a7f9b5e388e
ssmtkh18 <siegfriedst@mac.com>
2023-01-11 16:26:05 +0900

    Initial Commit

---
+++ NasaPics/NasaPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaPicsApp.swift
+//  NasaPics
+//
+//  Created by SieG on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct NasaPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
